1183b94a952a94077f1cd538db5dea51f37fc8b7
xdavalillo-cxc <74461025+xdavalillo-cxc@users.noreply.github.com>
2021-02-28 18:25:58 -0300

    cambiando nombre objeto resultuados

--- my-amaris-app/routes/primos.js
+++ my-amaris-app/routes/primos.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var Calculo = require('../clases/CalculoPrimarios.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var resultPrimarios = new Calculo();
-    res.render('result', { title: 'Resultado de calculo de numeros primos:', results: resultPrimarios.primarios(req.query.numero) });
+    var resultadosPrimarios = new Calculo();
+    res.render('result', { title: 'Resultado de calculo de numeros primos:', results: resultadosPrimarios.primarios(req.query.numero) });
 });
 
 module.exports = router;
--- my-amaris-app/test/CalculoPrimariosTest.js
+++ my-amaris-app/test/CalculoPrimariosTest.js
@@ -4,11 +4,11 @@ var assert = chai.assert;
 var Calculo = require('../clases/CalculoPrimarios.js');
 
 describe('calculoPrimarios', function() {
-    var resultPrimarios = new Calculo();
+    var resultadosPrimarios = new Calculo();
 
     describe('#primarios', function() {
         it("Los numeros primarios resultantes estan correctos", function() {
-            assert.deepEqual(resultPrimarios.primarios(10), [ 7, 5, 3, 2 ]);
+            assert.deepEqual(resultadosPrimarios.primarios(10), [ 7, 5, 3, 2 ]);
         });
     });
 });
